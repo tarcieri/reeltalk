c842ee266263cd6fecf8a418deddce5924eaecc4
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-01 03:28:05 -0800

    Basic message dispatch

--- lib/reeltalk/client.rb
+++ lib/reeltalk/client.rb
@@ -4,6 +4,7 @@ module Reeltalk
   class Client
     include Celluloid::IO
     include Celluloid::Logger
+    include Celluloid::Notifications
 
     attr_reader :socket
 
@@ -11,26 +12,36 @@ module Reeltalk
       @socket = socket
       @nickname = nil
 
+      subscribe 'chat', :handle_chat
       async.run
     end
 
     def run
       while message = JSON.parse(@socket.read)
-        dispatch message['action'], message
+        dispatch message
       end
     rescue EOFError
       info "#{nickname} disconnected"
       terminate
     end
 
-    def dispatch(action, params = {})
-      case action
+    def dispatch(message)
+      debug "Dispatching #{message.inspect}"
+
+      case message['action']
       when 'join'
-        @nickname = params['user']
+        @nickname = message['user']
         info "#{nickname} joined"
+        publish 'chat', message
+      when 'message'
+        publish 'chat', message.merge('user' => @nickname)
       else
         warn "unknown command '#{action}'"
       end
+    end
+
+    def handle_chat(_, message)
+      @socket << JSON.generate(message)
     end
 
     def nickname
